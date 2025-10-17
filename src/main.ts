929f7e989afc3b78aae925492079d587f7b56590
ngonza81 <ngonza81@ucsc.edu>
2025-10-17 21:37:34 +0000

    Added Undo and Redo buttons Added Container for buttons for all of buttons to be below canvas (Completed Step 4)

--- src/main.ts
+++ src/main.ts
@@ -15,15 +15,22 @@ const ctx = canvas.getContext("2d")!;
 canvas.width = 256;
 canvas.height = 256;
 
+// Button container (So buttons can all be under canvas)
+const buttonContainer = document.createElement("div");
+document.body.append(buttonContainer);
+
 // Mouse Drawing State
 let isDrawing = false;
 
-// Array of points
+// Array of points and redo stacks
 const drawing: { x: number; y: number }[][] = [];
+let redoStack: { x: number; y: number }[][] = [];
 
 // Mouse Events
 canvas.addEventListener("mousedown", (e) => {
   const newStroke = [{ x: e.offsetX, y: e.offsetY }];
+  console.log("New Stroke Started");
+  redoStack = []; // Clear redo History
   drawing.push(newStroke);
   isDrawing = true;
   canvas.dispatchEvent(new Event("drawing-changed"));
@@ -49,6 +56,32 @@ document.body.append(clearButton);
 clearButton.addEventListener("click", () => {
   drawing.length = 0; // clear array
   canvas.dispatchEvent(new Event("drawing-changed"));
+});
+
+// Undo Button
+const undoButton = document.createElement("button");
+undoButton.textContent = "Undo";
+document.body.append(undoButton);
+
+undoButton.addEventListener("click", () => {
+  if (drawing.length > 0) {
+    const undoStroke = drawing.pop()!;
+    redoStack.push(undoStroke);
+    canvas.dispatchEvent(new Event("drawing-changed"));
+  }
+});
+
+// Redo button
+const redoButton = document.createElement("button");
+redoButton.textContent = "Redo";
+document.body.append(redoButton);
+
+redoButton.addEventListener("click", () => {
+  if (redoStack.length > 0) {
+    const redoStroke = redoStack.pop()!;
+    drawing.push(redoStroke);
+    canvas.dispatchEvent(new Event("drawing-changed"));
+  }
 });
 
 // Observer: Redraw on changes
